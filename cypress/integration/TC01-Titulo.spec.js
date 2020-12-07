732bd1c5316edf44693de4f84e0ff232cdaffb4f
Nicolas Guilherme <nicolas.gmop@gmail.com>
2020-12-07 19:14:42 -0300

    att cypress

---
+++ cypress/integration/TC01-Titulo.spec.js
@@ -0,0 +1,8 @@
+describe('Acessar site e validar texto de apresentação', () => {
+  it('Acessar site', () => {
+    cy.visit('http://agapito-server.herokuapp.com/');
+  });
+  it('validar texto', () => {
+    cy.contains('Este é o site do Prof');
+  });
+});
